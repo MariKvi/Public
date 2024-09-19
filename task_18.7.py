e74ad568b952f0a15c009157b639325f50823490
MariKvi <marrista@mail.ru>
2024-09-19 09:26:52 +0300

    Update task_18.7.py

--- task_18.7.py
+++ task_18.7.py
@@ -27,8 +27,10 @@ print('''
         2. Вывести средний балл по всем предметам по каждому ученику
         3. Вывести все оценки по всем ученикам
         4. Вывести список должников по предметам
-        5. Редактировать оценки ученика по предмету
-        6. Выход из программы
+        5. Удалить и редактировать данные по оценкам, предметам и ученикам
+        6. Вывести информацию по всем оценкам для определенного ученика
+        7. Вывести средний балл по каждому предмету по определенному ученику
+        8. Выход из программы
         ''')
 
 while True:
@@ -87,24 +89,79 @@ while True:
                 print(f'\t{class_} - {needed_marks}')
 
     elif command == 5:
-        print('5. Редактировать оценки ученика по предмету')
-        # считываем имя ученика
-        student = input('Введите имя ученика: ')
-        # считываем название предмета
-        class_ = input('Введите предмет: ')
-        # считываем оценку
-        if student in students_marks.keys() and class_ in students_marks[student].keys():
-            # выводим все оценки ученика по предмету
-            print(f'Оценки {student} по предмету {class_}: {students_marks [student][class_]}')
-            edited_mark = int(input('Какую оценку нужно изменить: '))
-            students_marks[student][class_].remove(edited_mark)
-            new_mark = int(input('Введите новую оценку: '))
-            students_marks[student][class_].append(new_mark)
-            print(f'Оценки {student} по предмету {class_}: {students_marks[student][class_]}')
-        else:
-            print('ОШИБКА: неверное имя ученика или название предмета')
+        print('''
+                Выберите команду:
+                1. Удалить и редактировать данные по оценкам
+                2. Удалить и редактировать данные по ученикам
+                3. Удалить и редактировать данные по предметам
+                4. Выход из программы
+                   ''')
+        while True:
+            command = int(input('Введите команду: '))
+            if command == 1:
+                print('1. Удалить и редактировать данные по оценкам')
+                student = input('Введите имя ученика: ')
+                class_ = input('Введите предмет: ')
+                if student in students_marks.keys() and class_ in students_marks[student].keys():
+                    print(f'Оценки {student} по предмету {class_}: {students_marks [student][class_]}')
+                    edited_mark = int(input('Какую оценку нужно удалить: '))
+                    students_marks[student][class_].remove(edited_mark)
+                    new_mark = int(input('Введите новую оценку: '))
+                    students_marks[student][class_].append(new_mark)
+                    print(f'Актуальные оценки {student} по предмету {class_}: {students_marks[student][class_]}')
+                else:
+                    print('ОШИБКА: неверное имя ученика или название предмета')
+
+            if command == 2:
+                print('2. Удалить и редактировать данные по ученикам')
+                delete_student = input('Введите имя ученика, которого нужно удалить: ')
+                new_student = input('Введите имя нового ученика: ')
+                if delete_student in students:
+                    del students_marks[delete_student]
+                else:
+                    print('ОШИБКА: неверное имя ученика')
+                students_marks[new_student] = classes
+                print(f'Ученик {delete_student} удален из списка. Добавлен новый ученик {new_student}')
+                print(f'Список всех учеников: {list(students_marks.keys())}')
+
+            if command == 3:
+                print('3. Удалить и редактировать данные по предметам')
+                student = input('Введите имя ученика для удаления предмета: ')
+                delete_class = input('Введите предмет, который нужно удалить: ')
+                new_class = input('Введите название нового предмета: ')
+                if student in students_marks.keys() and delete_class in students_marks[student].keys():
+                    students_marks[student][new_class] = students_marks[student][delete_class]
+                    students_marks[student].pop(delete_class)
+                    print(f'Список всех предметов после изменений: {students_marks}')
+                else:
+                    print('ОШИБКА: неверное имя ученика или название предмета')
+
+            elif command == 4:
+                print('4. Выход в предыдущее меню')
+                break
 
     elif command == 6:
-        print('6. Выход из программы')
+        print('6. Вывести информацию по всем оценкам для определенного ученика')
+        student = input('Введите имя ученика: ')
+        if student in students_marks.keys():
+            print(f'Оценки {student} по всем предметам:')
+            for class_ in classes:
+                print(f'\t{class_} - {students_marks[student][class_]}')
+        else:
+            print('ОШИБКА: неверное имя ученика')
+
+    elif command == 7:
+        print('7. Вывести средний балл по каждому предмету по определенному ученику')
+        student = input('Введите имя ученика: ')
+        if student in students_marks.keys():
+            for class_ in classes:
+                marks_sum = sum(students_marks[student][class_])
+                marks_count = len(students_marks[student][class_])
+                print(f'\t{class_} - {marks_sum // marks_count}')
+        else:
+            print('ОШИБКА: неверное имя ученика')
+
+    elif command == 8:
+        print('8. Выход из программы')
         break
 
